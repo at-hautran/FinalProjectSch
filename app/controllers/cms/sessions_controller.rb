8deb5de0edc1272d637610db41baee4921a95b35
Tran Hau <haut@asiantech.vn>
2017-04-08 15:16:01 +0700

    controller sessions was created

---
+++ app/controllers/cms/sessions_controller.rb
@@ -0,0 +1,2 @@
+class Cms::SessionsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  namespace :cms do
+    resource :sessions
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
