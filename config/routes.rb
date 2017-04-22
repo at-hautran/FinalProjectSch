5bf7540017057b23bc21638e4e43b5f54dfb4302
Tran Hau <haut@asiantech.vn>
2017-04-22 19:20:05 +0700

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :rooms
   end
   resources :rooms, only: %w[show index]
-  resources :customers
+  resources :customers, only: %w[show index]
   root 'homepages#home'
   # get 'homepages/booking', to: 'homepages#booking'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
