e652bdd49fd520c7c78ec32a67aca1bc75abb441
Tran Hau <haut@asiantech.vn>
2017-04-27 21:57:58 +0700

    validate bookings model

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,20 +1,23 @@
 class BookingsController < ApplicationController
   def new
     @room = Room.find(params[:room_id])
+    @bookings = @room.bookings.build
   end
 
   def create
     # Booking.book_ticket(customer_params, booking_params)
     customer = Customer.create customer_params
-    booking = customer.bookings.build booking_params
-    if current_user && current_user.role == 'admin'
-      booking.user_id = current_user.id
-      booking.status = 'accept'
-    end
-    if booking.save
-      # redirect_to
+    @booking = customer.bookings.build booking_params
+    @booking.status = 'waiting'
+    # if current_user && current_user.role == 'admin'
+    #   booking.user_id = current_user.id
+    #   booking.status = 'accept'
+    # end
+    if @booking.save
+      redirect_to
     else
-      # render
+      @room = Room.find(booking_params[:room_id])
+      render action: :new
     end
   end
 
