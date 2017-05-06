00467b2ee4f9c10ea906f830f82864f20359919f
Tran Hau <haut@asiantech.vn>
2017-05-06 12:44:40 +0700

    config I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
   protect_from_forgery with: :exception
   skip_before_filter :verify_authenticity_token
+
   include ApplicationHelper
   def authenticated_user
     if current_user
@@ -9,4 +12,15 @@ class ApplicationController < ActionController::Base
       'Non'
     end
   end
+
+  private
+
+    def set_locale
+      binding.pry
+      I18n.set_locale = params[:locale] || I18n.default_locale
+    end
+
+    def default_url_options(options = {})
+      { locale: I18n.locale }.merge options
+    end
 end
