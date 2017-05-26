aa1b70ef4e14580536fa425421235da04adde452
Tran Hau <haut@asiantech.vn>
2017-05-26 09:47:30 +0700

    a

--- app/controllers/cms/bookings_controller.rb
+++ app/controllers/cms/bookings_controller.rb
@@ -87,7 +87,57 @@ class Cms::BookingsController < Cms::ApplicationController
   end
 
   def show
-    @booking = Booking.find(params[:id])
+    item = InvoicePrinter::Document::Item.new(
+      name: 'Web consultation',
+      quantity: nil,
+      unit: 'hours',
+      price: '$ 25',
+      tax: '$ 1',
+      amount: '$ 100'
+    )
+
+    invoice = InvoicePrinter::Document.new(
+      number: '201604030001',
+      provider_name: 'Business s.r.o.',
+      provider_tax_id: '56565656',
+      provider_tax_id2: '465454',
+      provider_street: 'Rolnicka',
+      provider_street_number: '1',
+      provider_postcode: '747 05',
+      provider_city: 'Opava',
+      provider_city_part: 'Katerinky',
+      provider_extra_address_line: 'Czech Republic',
+      purchaser_name: 'Adam',
+      purchaser_tax_id: '',
+      purchaser_tax_id2: '',
+      purchaser_street: 'Ostravska',
+      purchaser_street_number: '1',
+      purchaser_postcode: '747 70',
+      purchaser_city: 'Opava',
+      purchaser_city_part: '',
+      purchaser_extra_address_line: '',
+      issue_date: '19/03/3939',
+      due_date: '19/03/3939',
+      subtotal: '175',
+      tax: '5',
+      tax2: '10',
+      tax3: '20',
+      total: '$ 200',
+      bank_account_number: '156546546465',
+      account_iban: 'IBAN464545645',
+      account_swift: 'SWIFT5456',
+      items: [item],
+      note: 'A note...'
+    )
+
+      respond_to do |format|
+    format.pdf {
+      @pdf = InvoicePrinter.render(
+        document: invoice
+      )
+      send_data @pdf, type: 'application/pdf', disposition: 'inline'
+    }
+  end
   end
 
   def edit
